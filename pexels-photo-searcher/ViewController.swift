37657a5a72e47245e48be3f5e4348f528a4aade8
Yusuke Miyata <yskmyt.dev@gmail.com>
2021-11-11 23:20:57 +0900

    Initial Commit

---
+++ pexels-photo-searcher/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  pexels-photo-searcher
+//
+//  Created by Yusuke Miyata on 2021/11/11.
+//  Copyright © 2021 Yusuke Miyata. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ pexels-photo-searcherTests/pexels_photo_searcherTests.swift
@@ -0,0 +1,34 @@
+//
+//  pexels_photo_searcherTests.swift
+//  pexels-photo-searcherTests
+//
+//  Created by Yusuke Miyata on 2021/11/11.
+//  Copyright © 2021 Yusuke Miyata. All rights reserved.
+//
+
+import XCTest
+@testable import pexels_photo_searcher
+
+class pexels_photo_searcherTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
