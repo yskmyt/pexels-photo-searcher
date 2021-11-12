d481eb628e7a86238ce5ffbbe1f612546d2e4130
Yusuke Miyata <yskmyt.dev@gmail.com>
2021-11-12 21:43:10 +0900

    add PhotoSearchViewController and change root storyboard

---
+++ pexels-photo-searcher/Controller/PhotoSearchViewController.swift
@@ -0,0 +1,19 @@
+//
+//  PhotoSearchViewController.swift
+//  pexels-photo-searcher
+//
+//  Created by Yusuke Miyata on 2021/11/12.
+//  Copyright © 2021 Yusuke Miyata. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+
+final class PhotoSearchViewController: UIViewController {
+    @IBOutlet private weak var searchBar: UISearchBar!
+    @IBOutlet private weak var collectionView: UICollectionView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
